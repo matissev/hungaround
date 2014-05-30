a7db512aa1d3488d590431954252967360144c76
matissev <Simon@MacBook-de-Simon-Vrignaud.local>
2014-05-30 13:13:00 +0200

    Mise en place du menu déroulant #1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-function merde (){
-	alert("jack");
+
+
+function toggleNotif() {
+	var menu = document.getElementById("menuNotif");
+
+	switch (menu.style.display)
+	case "block" :
+		menu.style.display = "none";
+		break;
+	case "none" :
+		menu.style.display = "block";
+		break;
 }
