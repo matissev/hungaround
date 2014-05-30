7400f852161e772cb468c5daee8f8d2496b13917
matissev <Simon@MacBook-de-Simon-Vrignaud.local>
2014-05-30 14:01:07 +0200

    Translate notification

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@
 function toggleNotif() {
 	var menu = document.getElementById("menuNotif");
 
-	switch (menu.style.display)
-	case "block" :
-		menu.style.display = "none";
+	switch (menu.className) {
+	case "nav-profil-menuDeroulant open" :
+		menu.className = "nav-profil-menuDeroulant close";
 		break;
-	case "none" :
-		menu.style.display = "block";
+	case "nav-profil-menuDeroulant close" :
+		menu.className = "nav-profil-menuDeroulant open";
 		break;
+	}
 }
