526dc315944cea94f7c89c421fc8efb0c917048f
matissev <Simon@MacBook-de-Simon.local>
2014-06-14 20:25:45 +0200

    Hudge Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+var shown = false;
+var step = 0;
+
 
 jQuery(document).ready(function($) {
+
+	$('.my-sticky-element').waypoint('sticky');
+	$('.infinite-container').waypoint('infinite');
 
 	// =========================== FILTRES MOSAIC ===========================
 
@@ -26,23 +32,102 @@ jQuery(document).ready(function($) {
 
 	// =========================== GOOGLE MAPS API ===========================
 
+	var timer = false;
 	function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(42.71760, 0.59314),
+          center: new google.maps.LatLng(44.770137, 6.965332),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.TERRAIN
         };
-        var map = new google.maps.Map(document.getElementById("largeMap"), mapOptions);
+        map = new google.maps.Map(document.getElementById("largeMap"), mapOptions);
         map.setTilt(45);
   		map.setHeading(90);
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 
 
-
     // =========================== TOGGLES ===========================
 
     $(".nav-profil-photo").click(function(event) { $("#menuNotif").toggleClass("open"); })
+
+    $(".showArticle").click(function(event) {
+    	$(".article").toggleClass("toggleArticle");
+    	$("main").toggleClass("away");
+    	$("#largeMap").toggleClass("littlemap");
+    	$(".closeArticle").toggleClass("toggleCloser");
+    	shown = !shown;
+
+    	if ( shown == true ) {
+    		LatLng1 = new google.maps.LatLng(48.393764, 2.605991);
+    		LatLng2 = new google.maps.LatLng(48.39365, 2.638435);
+    		LatLng3 = new google.maps.LatLng(48.382478, 2.651482);
+    		map.setCenter(LatLng1);
+    		map.setZoom(14);
+
+			timer = setTimeout(function() {
+				google.maps.event.trigger(map, 'resize');
+	  			map.panTo(LatLng1);
+
+	  			var markerArticle1 = new google.maps.Marker({
+		    		position: LatLng1,
+		    		map: map,
+		    		title: "Hello World!",
+		    		icon: 'img/icones/map-marker.svg'
+				});
+
+				var markerArticle2 = new google.maps.Marker({
+		    		position: LatLng2,
+		    		map: map,
+		    		title: "Hello World!",
+		    		icon: 'img/icones/map-marker.svg'
+				});
+
+				var markerArticle3 = new google.maps.Marker({
+		    		position: LatLng3,
+		    		map: map,
+		    		title: "Hello World!",
+		    		icon: 'img/icones/map-marker.svg'
+				});
+
+				var lineSymbol = {
+    				path: 'M 0,-1 0,1',
+    				strokeOpacity: 1,
+    				scale: 4
+  				};
+
+				var flightPlanCoordinates = [
+    				LatLng1,
+    				LatLng2,
+    				LatLng3
+  				];
+
+				var flightPath = new google.maps.Polyline({
+    				path: flightPlanCoordinates,
+    				icons: [{
+    				  icon: lineSymbol,
+    				  offset: '0',
+    				  repeat: '20px'
+    				}],
+    				geodesic: true,
+    				strokeColor: '#e64c27',
+    				strokeOpacity: 0,
+    				strokeWeight: 3
+    			});
+
+    			flightPath.setMap(map);
+			}, 1000);
+    	}
+
+    	else {
+    		initialize();
+
+    		timer = setInterval(function() {
+    			google.maps.event.trigger(map, 'resize');
+    			step++ ;
+    			if (step == 2) { clearInterval(timer); }
+    		}, 500);
+    	}
+    })
 
     $(".nav-register, .fermeture").click(function(event) { $(".popupContainer").toggleClass("show"); })
 
@@ -57,7 +142,26 @@ jQuery(document).ready(function($) {
 		}
 	})
 
-    // =========================== AJAXIFY ============================
+    // =========================== WAYPOINTS ============================
+
+	$('#marker1').waypoint(function() {
+  		map.panTo(LatLng1);
+	}, {context: '.article'});
+
+	$('#marker2-1').waypoint(function() {
+  		map.panTo(LatLng2);
+	}, {context: '.article'});
+
+	$('#marker2-2').waypoint(function() {
+  		map.panTo(LatLng2);
+	}, {context: '.article'});
+
+	$('#marker3').waypoint(function() {
+  		map.panTo(LatLng3);
+	}, {context: '.article'});
+
+	$('.infinite-container').waypoint('infinite');
+	$('.my-sticky-element').waypoint('sticky');
 
 });
 
