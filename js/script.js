522821296489877d3eca9143e976a307ac26a90e
matissev <Simon@MacBook-de-Simon.local>
2014-06-16 15:15:17 +0200

    Map OK

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 var shown = false;
 var step = 0;
-var blueMarkersPosition = ["43.909806, 6.225070", "44.193841, 6.192950", "44.039483, 6.696849", "43.959635, 5.573183", "43.600241, 6.113721", "43.830815, 7.071214", "44.057757, 4.765858", "43.933368, 4.305138", "48.393764, 2.605991", "48.39365, 2.638435", "48.382478, 2.651482", "43.373566, 2.440705", "42.892971, 0.935572", "42.857416, -0.555427", "43.012346, -1.168948", "43.088416, -1.842834", "42.690938, -1.145438", "42.362094, -0.207881", "41.995003, 1.170606"];
-var blueMarkers = 0;
+var blueMarkersPosition = [
+	{ "lat" : "43.909806" , "long" : "6.225070" },
+	{ "lat" : "44.193841" , "long" : "6.192950" },
+	{ "lat" : "44.039483" , "long" : "6.696849" },
+	{ "lat" : "43.959635" , "long" : "5.573183" },
+	{ "lat" : "43.600241" , "long" : "6.113721" },
+	{ "lat" : "43.830815" , "long" : "7.071214" },
+	{ "lat" : "44.057757" , "long" : "4.765858" },
+	{ "lat" : "43.933368" , "long" : "4.305138" },
+	{ "lat" : "48.393764" , "long" : "2.605991" },
+	{ "lat" : "48.39365" , "long" : "2.638435" },
+	{ "lat" : "48.382478" , "long" : "2.651482" },
+	{ "lat" : "43.373566" , "long" : "2.440705" },
+	{ "lat" : "42.892971" , "long" : "0.935572" },
+	{ "lat" : "42.857416" , "long" : "-0.555427" },
+	{ "lat" : "43.012346" , "long" : "-1.168948" },
+	{ "lat" : "43.088416" , "long" : "-1.842834" },
+	{ "lat" : "42.690938" , "long" : "-1.145438" },
+	{ "lat" : "42.362094" , "long" : "-0.207881" },
+	{ "lat" : "41.995003" , "long" : "1.170606" }
+];
+var blueMarkers = [];
 var marker = [];
 
 jQuery(document).ready(function($) {
@@ -48,12 +68,12 @@ jQuery(document).ready(function($) {
   		map.setHeading(90);
 
   		for (var i = 0; i < blueMarkersPosition.length; i++) {
-  			blueMarkers = new google.maps.LatLng(blueMarkersPosition[i]);
+  			blueMarkers[i] = new google.maps.LatLng(blueMarkersPosition[i].lat, blueMarkersPosition[i].long);
   			marker[i] = new google.maps.Marker({
 	    		position: blueMarkers[i],
 	    		map: map,
 	    		title: "Hello World!",
-	    		icon: 'img/icones/map-marker.svg'
+	    		icon: 'img/icones/recommande.svg'
 			});
   		}
     }
