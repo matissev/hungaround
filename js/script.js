b48adc8d4dfc760f3df084cdc73445483f485018
matissev <Simon@MacBook-de-Simon.local>
2014-06-17 22:42:19 +0200

    Virage des éléments création.html

--- js/script.js
+++ js/script.js
@@ -410,19 +410,19 @@ jQuery(document).ready(function($) {
     $(".textBut").click(function(event) {
     	$(".slides").append('<div class="slide newslide ajoutText"> <div class="ajoutTitre"> <input type="text" name="fname" placeholder="Titre"/> </div> <div class="ajoutParagraphe"> <textarea name="textarea" rows="10" cols="50" placeholder="Saisir un texte ici."></textarea> </div> <div class="ajoutLieuText"> <input type="text" name="fname" placeholder="Ajouter une localisation"/> </div> <div class="handle"></div> <div class="deleteBut"></div> </div>');
     	reloadSlides();
-    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:1000});
+    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:500});
     })
 
     $(".imgBut").click(function(event) {
     	$(".slides").append('<div class="slide newslide ajoutFile"> <div class="ajoutImg"> <input type="file" name="pic" accept="image/*" placeholder="Ajouter une localisation"/> </div> <div class="ajoutLieuImg"> <input type="text" name="fname" placeholder="Ajouter une localisation"/> </div> <div class="handle"></div> <div class="deleteBut"></div> </div>');
     	reloadSlides();
-    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:1000});
+    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:500});
     })
 
     $(".vidBut").click(function(event) {
     	$(".slides").append('<div class="slide newslide ajoutFile"> <div class="ajoutVid"> <input type="file" name="vid" accept="video/*"/> </div> <div class="ajoutLieuVid"> <input type="text" name="fname" placeholder="Ajouter une localisation"/> </div> <div class="handle"></div> <div class="deleteBut"></div> </div> </div>');
     	reloadSlides();
-    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:1000});
+    	$('#bazinga').animatescroll({element:'.creation', scrollSpeed:500});
     })
 
     function reloadSlides() {
