3463169c2e7bcda482d4ad89058f28a5c098a985
matissev <Simon@MacBook-de-Simon.local>
2014-06-17 15:46:21 +0200

    Bim le drag n drop

--- js/draggable.js
+++ js/draggable.js
@@ -1,7 +1,7 @@
 docReady( function() {
   var container = document.querySelector('.slides');
 var pckry = new Packery( container, {
-  rowHeight: 140,
+  rowHeight: 130,
   columnWidth: 1000
 });
 
@@ -10,7 +10,9 @@ var itemElems = pckry.getItemElements();
 for ( var i=0, len = itemElems.length; i < len; i++ ) {
   var elem = itemElems[i];
   // make element draggable with Draggabilly
-  var draggie = new Draggabilly( elem );
+  var draggie = new Draggabilly( elem, {
+    handle: '.handle'
+  } );
   // bind Draggabilly events to Packery
   pckry.bindDraggabillyEvents( draggie );
 }
